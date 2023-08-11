ef3650fdaf27379cfaa8592a756aa1c7638b49f9
Llala <rene.raboshakga@gmail.com>
2023-08-11 13:54:28 -0700

    Completed Tasks 0 - 12

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int n;
+
+	for (n = 0, n < 26; n++)
+	{
+		putchar(alp[n]);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int n;
+
+	for (n = 0; n < 52; n++)
+	{
+		putchar(alp[52]);
+	}
+	putchar('\n');
+	return (0);
+}
