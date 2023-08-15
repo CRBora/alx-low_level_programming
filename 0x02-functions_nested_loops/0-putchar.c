85b2e0a0950f773fe86d0300203e865cb2731dd7
Llala <rene.raboshakga@gmail.com>
2023-08-15 10:26:43 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - finction to print char to stdout
+ *
+ * Return: _putchar printed to stdout
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
