ef0717e511f18b9f426a2ff058c355361cd61443
Llala <rene.raboshakga@gmail.com>
2023-08-20 02:39:40 -0700

    Task 10 simple function to add to integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - function to add 2 integers and print the result
+ * @l: first integer
+ * @k: second int
+ * Return: sum of int l and int k to stdout
+ */
+int add(int l, int k)
+{
+	return (l + k);
+}
