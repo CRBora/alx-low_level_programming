974b1ebfae36d0e0886c476ec270f302137b2f78
Llala <rene.raboshakga@gmail.com>
2023-09-05 23:21:17 +0200

    Task 1, returning allocated memory containing copy o string given as a parameter

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup - function that returns a pointer to a newly allocated
+ * space in memory which contains a copy of the string given
+ * as a parameter
+ * @str: pointer to input string
+ *
+ * Return: pointer to string copy, otherwise NULL if memory
+ * isinsufficient
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int l, k = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	l = 0;
+	while (*str)
+		l++;
+
+	p = malloc(sizeof(char) * (l + 1));
+	if (p == NULL)
+		return (NULL);
+
+	for (k = 0; str[k]; k++)
+		p[k] = str[k];
+
+	return (p);
+}
