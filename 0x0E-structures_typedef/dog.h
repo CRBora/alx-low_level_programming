473594949c2c0b1f91dc4ed00102c03b00d85330
Llala <rene.raboshakga@gmail.com>
2023-09-11 21:49:12 +0200

    Task 0 correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - information for a dog
+ * @name:pointer to name of dog
+ * @age: age of dog
+ * @owner: pointer to dog owner
+ */
+
 struct dog
 {
 	char *name;
