b5ea6a9ddfed7710fddcdbdb24f158785eebbebb
Llala <rene.raboshakga@gmail.com>
2023-09-13 20:20:04 +0200

    Task 0 second correction

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,28 +2,6 @@
 #include <stdio.h>
 
 /**
- * rev_string - function that reverses a string
- * @s: pointer to string to be reversed
- */
-void rev_string(char *s)
-{
-	char rev = s[0];
-	int r = 0;
-	int l;
-
-	while (s[r] != '\0')
-		r++;
-
-	for (l = 0; l < r; l++)
-	{
-		r--;
-		rev = s[l];
-		s[l] = s[r];
-		s[r] = rev;
-	}
-}
-
-/**
  * print_name - function that prints a name
  * @name: pointer to string
  * @f: pointer to a function
